c5d7ff25c39bd830b0f00d2cb9db1886bf195e04
Shayna Kaushal <shaynakaushal@gmail.com>
2022-10-17 14:55:15 -0500

    init xcode project

---
+++ FindMyHerd/FindMyHerd/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FindMyHerd
+//
+//  Created by Shayna Kaushal on 10/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindMyHerd/FindMyHerd/FindMyHerdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyHerdApp.swift
+//  FindMyHerd
+//
+//  Created by Shayna Kaushal on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FindMyHerdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
