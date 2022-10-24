c0af0daa1dba41d74e1ce27dbd9db89fdb61fe08
Shayna Kaushal <shaynakaushal@gmail.com>
2022-10-24 14:36:25 -0500

    connected firebase

--- FindMyHerd/FindMyHerd/FindMyHerdApp.swift
+++ FindMyHerd/FindMyHerd/FindMyHerdApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import UIKit
+import FirebaseCore
 
-@main
-struct FindMyHerdApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+//@main
+//struct FindMyHerdApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
---
+++ FindMyHerd/FindMyHerd/user_info.swift
@@ -0,0 +1,10 @@
+//
+//  user_info.swift
+//  FindMyHerd
+//
+//  Created by Shayna Kaushal on 10/24/22.
+//
+
+import Foundation
+
+
