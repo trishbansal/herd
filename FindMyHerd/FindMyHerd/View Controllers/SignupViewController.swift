6abebd7815fde7f9868c97b3f3784df5dbe49b7e
Shayna Kaushal <shaynakaushal@gmail.com>
2022-10-28 15:46:39 -0500

    made login,signup,home pages + buttons to test

---
+++ FindMyHerd/FindMyHerd/View Controllers/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  FindMyHerd
+//
+//  Created by Shayna Kaushal on 10/28/22.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
